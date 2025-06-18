d3c60193b53088c6a4c14e99f5ed6ede5c96a12c
Mustafa Rashid <mustafa.rashid.abbas@gmail.com>
2025-06-18 11:36:05 +0300

    edit image url

--- src/components/TitleComponent/TitleComponent.tsx
+++ src/components/TitleComponent/TitleComponent.tsx
@@ -7,7 +7,7 @@ interface TitleComponentProps {
 const TitleComponent = ({ heading, subheading }: TitleComponentProps) => {
   return (
     <>
-      <img src="task5/images/Auth/Logo.png" alt="logo" className="mb-3 mb-lg-4 w" />
+      <img src="/task5/images/Auth/Logo.png" alt="logo" className="mb-3 mb-lg-4 w" />
       <p className="fw-semibold text-black fs-22 mb-1 mb-lg-2">{heading}</p>
       <p className="fs-14 fw-normal mb-2 mb-lg-4 text-gray text-center">{subheading}</p>
     </>
